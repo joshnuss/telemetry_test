e6d44ebbaa29e018247955831b585d3cfcffdb6c
Josh Nussbaum <joshnuss@gmail.com>
2019-11-19 21:52:49 -0500

    Adds timestamp

--- lib/telemetry_test/thing.ex
+++ lib/telemetry_test/thing.ex
@@ -2,6 +2,7 @@ defmodule TelemetryTest.Thing do
   def do_stuff do
     event = [:spaceship, :engines]
     measurements = %{
+      timestamp: DateTime.utc_now,
       count: 10,
       speed: 10_000_000_000,
       check_engine_light: :on
