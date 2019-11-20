02cdea133ccb3917ce447f209a0df1deeb270630
Josh Nussbaum <joshnuss@gmail.com>
2019-11-19 21:52:01 -0500

    Refactors telemetry execution

--- lib/telemetry_test/thing.ex
+++ lib/telemetry_test/thing.ex
@@ -1,5 +1,12 @@
 defmodule TelemetryTest.Thing do
   def do_stuff do
-    :ok = :telemetry.execute([:spaceship, :engines], %{count: 10, speed: 10_000_000_000, check_engine_light: :on})
+    event = [:spaceship, :engines]
+    measurements = %{
+      count: 10,
+      speed: 10_000_000_000,
+      check_engine_light: :on
+    }
+
+    :ok = :telemetry.execute(event, measurements)
   end
 end
